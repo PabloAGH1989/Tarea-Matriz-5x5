ab9dcb35c1dee73981572767306a5484cf899af5
PabloAGH1989 <huellasccmna2015@gmail.com>
2026-03-22 22:13:09 -0500

    Add files via upload

---
+++ Semana14-Doble de un Número.py
@@ -0,0 +1,7 @@
+def doble(n1):
+    resultado = n1 * 2
+    return resultado
+print("Calcular el doble de un número")
+n1 = float(input("Ingrese un número: "))
+resultado = doble(n1)       
+print(f"El doble de {n1} es {resultado}")
